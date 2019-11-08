8d2e8d8afd8fe0f2c1dd7f8b7279df689ae0fcea
Mina Milosevic <mikimina98@gmail.com>
2019-11-08 21:12:11 +0100

    prva verzija

---
+++ akcije.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+
+    $("#p1mat").hide();
+    $("#p2mat").hide();
+    $("#engl1mat").hide();
+    $("#engl2mat").hide();
+    $("#uor1mat").hide();
+    $("#uor2mat").hide();
+    $("#aispmat").hide();
+    $("#osmat").hide();
+    $("#oasmat").hide();
+    $("#a2mat").hide();
+    $("#alg1mat").hide();
+    $("#geommat").hide();
+    $("#ppjmat").hide();
+    $("#rgmat").hide();
+    $("#unmmat").hide();
+    $("#rbpmat").hide();
+
+    $("#p1").on("click", function(){
+        $("#p1mat").slideToggle("slow");
+    });
+
+    $("#p2").on("click", function(){
+        $("#p2mat").slideToggle("slow");
+    });
+
+    $("#engl1").on("click", function(){
+        $("#engl1mat").slideToggle("slow");
+    });
+
+    $("#engl2").on("click", function(){
+        $("#engl2mat").slideToggle("slow");
+    });
+
+    $("#uor1").on("click", function(){
+        $("#uor1mat").slideToggle("slow");
+    });
+
+    $("#uor2").on("click", function(){
+        $("#uor2mat").slideToggle("slow");
+    });
+
+    $("#aisp").on("click", function(){
+        $("#aispmat").slideToggle("slow");
+    });
+
+    $("#os").on("click", function(){
+        $("#osmat").slideToggle("slow");
+    });
+
+    $("#a2").on("click", function(){
+        $("#a2mat").slideToggle("slow");
+    });
+
+    $("#alg1").on("click", function(){
+        $("#alg1mat").slideToggle("slow");
+    });
+
+    $("#oas").on("click", function(){
+        $("#oasmat").slideToggle("slow");
+    });
+
+    $("#geom").on("click", function(){
+        $("#geommat").slideToggle("slow");
+    });
+
+    $("#rg").on("click", function(){
+        $("#rgmat").slideToggle("slow");
+    });
+
+    $("#ppj").on("click", function(){
+        $("#ppjmat").slideToggle("slow");
+    });
+
+    $("#rbp").on("click", function(){
+        $("#rbpmat").slideToggle("slow");
+    });
+
+    $("#unm").on("click", function(){
+        $("#unmmat").slideToggle("slow");
+    });
+
+});
